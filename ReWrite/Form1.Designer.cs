6e768194d36e94629b10b769f3775246c975577d
Laffey-Chan <kankainenleevi@gmail.com>
2025-02-12 15:15:49 +0200

    Fixed the design of the rewrite

--- ReWrite/Form1.Designer.cs
+++ ReWrite/Form1.Designer.cs
@@ -53,12 +53,12 @@
             TabControl1.Location = new Point(0, 0);
             TabControl1.Name = "TabControl1";
             TabControl1.SelectedIndex = 0;
-            TabControl1.Size = new Size(894, 558);
+            TabControl1.Size = new Size(668, 321);
             TabControl1.TabIndex = 10;
             // 
             // TabPage1
             // 
-            TabPage1.BackColor = SystemColors.GrayText;
+            TabPage1.BackColor = Color.Transparent;
             TabPage1.Controls.Add(ListBox1);
             TabPage1.Controls.Add(Button1);
             TabPage1.Controls.Add(Label2);
@@ -69,25 +69,24 @@
             TabPage1.Location = new Point(4, 24);
             TabPage1.Name = "TabPage1";
             TabPage1.Padding = new Padding(3);
-            TabPage1.Size = new Size(886, 530);
+            TabPage1.Size = new Size(660, 293);
             TabPage1.TabIndex = 0;
             TabPage1.Text = "TabPage1";
             // 
             // ListBox1
             // 
-            ListBox1.Font = new Font("Segoe UI", 12F);
             ListBox1.FormattingEnabled = true;
-            ListBox1.ItemHeight = 21;
-            ListBox1.Location = new Point(34, 126);
+            ListBox1.ItemHeight = 15;
+            ListBox1.Location = new Point(3, 94);
             ListBox1.Name = "ListBox1";
-            ListBox1.Size = new Size(539, 298);
+            ListBox1.Size = new Size(657, 199);
             ListBox1.TabIndex = 6;
             // 
             // Button1
             // 
             Button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             Button1.AutoSize = true;
-            Button1.Location = new Point(776, 60);
+            Button1.Location = new Point(550, 60);
             Button1.Name = "Button1";
             Button1.Size = new Size(104, 25);
             Button1.TabIndex = 3;
@@ -108,7 +107,7 @@
             // 
             UpdateLinkButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             UpdateLinkButton.AutoSize = true;
-            UpdateLinkButton.Location = new Point(775, 6);
+            UpdateLinkButton.Location = new Point(549, 6);
             UpdateLinkButton.Name = "UpdateLinkButton";
             UpdateLinkButton.Size = new Size(105, 25);
             UpdateLinkButton.TabIndex = 2;
@@ -119,7 +118,7 @@
             // 
             ComboBoxLanguage.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             ComboBoxLanguage.FormattingEnabled = true;
-            ComboBoxLanguage.Location = new Point(579, 7);
+            ComboBoxLanguage.Location = new Point(353, 7);
             ComboBoxLanguage.Name = "ComboBoxLanguage";
             ComboBoxLanguage.Size = new Size(150, 23);
             ComboBoxLanguage.TabIndex = 1;
@@ -148,7 +147,7 @@
             TabPage2.Location = new Point(4, 24);
             TabPage2.Name = "TabPage2";
             TabPage2.Padding = new Padding(3);
-            TabPage2.Size = new Size(886, 530);
+            TabPage2.Size = new Size(660, 293);
             TabPage2.TabIndex = 1;
             TabPage2.Text = "TabPage2";
             TabPage2.UseVisualStyleBackColor = true;
@@ -160,14 +159,14 @@
             DataGridView1.Dock = DockStyle.Fill;
             DataGridView1.Location = new Point(3, 3);
             DataGridView1.Name = "DataGridView1";
-            DataGridView1.Size = new Size(880, 524);
+            DataGridView1.Size = new Size(654, 287);
             DataGridView1.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(894, 558);
+            ClientSize = new Size(668, 321);
             Controls.Add(TabControl1);
             Name = "Form1";
             Text = "Form1";
